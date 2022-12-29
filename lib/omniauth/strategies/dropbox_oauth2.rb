d432a74f48c3b7639a7dd6c1a922d640d5ec88d1
Justin <rectifyer@users.noreply.github.com>
2022-12-29 08:44:48 -0800

    fix callback_url
    https://github.com/bamorim/omniauth-dropbox-oauth2/pull/5/commits/35743bb095aeda8896a1a02d71a79589a8efb3d7

--- lib/omniauth/strategies/dropbox_oauth2.rb
+++ lib/omniauth/strategies/dropbox_oauth2.rb
@@ -39,12 +39,16 @@ module OmniAuth
         @raw_info ||= MultiJson.decode(response.body)
         # @raw_info ||= MultiJson.decode(access_token.get('/2/users/get_current_account').body)
       end
+      
+      def callback_url_without_query_string
+         full_host + script_name + callback_path
+       end
 
       def callback_url
         if @authorization_code_from_signed_request
           ''
         else
-          options[:callback_url] || super
+          options[:callback_url] || callback_url_without_query_string
         end
       end
     end
